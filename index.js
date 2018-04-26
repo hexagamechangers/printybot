46756d035b8e019793ddbab51bc256f1059a7019
hexagamechangers_prakash <38518631+hexagamechangers@users.noreply.github.com>
2018-04-26 15:34:21 +0530

    slack messaging example
    slack messaging example

--- index.js
+++ index.js
@@ -93,20 +93,45 @@ server.post('/get-movie-details', (req, res) => {
 
                 "slack": {
 
-                    //"text": "This is a text response for Slack."
-                    "card": {
-                        "title": "Avengers Inifnity War - Part 1",
-                        "subtitle": "Part - 1",
-                        "imageUri": "https://i.ytimg.com/vi/y_DMuo9Oefc/maxresdefault.jpg",
-                        "buttons": [
+                    "text": "Would you like to play a game?",
+                    "attachments": [
+                        {
+                            "text": "Choose a game to play",
+                            "fallback": "You are unable to choose a game",
+                            "callback_id": "wopr_game",
+                            "color": "#3AA3E3",
+                            "attachment_type": "default",
+                            "actions": [
                                 {
-                                    "text": "Click Me !!",
-                                    "postback": "https://i.ytimg.com/vi/y_DMuo9Oefc/maxresdefault.jpg"
+                                    "name": "game",
+                                    "text": "Chess",
+                                    "type": "button",
+                                    "value": "chess"
+                                },
+                                {
+                                    "name": "game",
+                                    "text": "Falken's Maze",
+                                    "type": "button",
+                                    "value": "maze"
+                                },
+                                {
+                                    "name": "game",
+                                    "text": "Thermonuclear War",
+                                    "style": "danger",
+                                    "type": "button",
+                                    "value": "war",
+                                    "confirm": {
+                                        "title": "Are you sure?",
+                                        "text": "Wouldn't you prefer a good game of chess?",
+                                        "ok_text": "Yes",
+                                        "dismiss_text": "No"
+                                    }
                                 }
-                        ]
-                    }
-
+                            ]
+                        }
+                    ]
                 }
+
 
             },
 
